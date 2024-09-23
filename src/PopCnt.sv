abd237d93d92f84cbee3171c70cc89924b4fff44
Mathis Salmen <mathis.salmen@matsal.de>
2024-09-23 12:13:20 +0200

    simplify popcnt for synthesis

--- src/PopCnt.sv
+++ src/PopCnt.sv
@@ -14,6 +14,21 @@ wire[SIZE_POW2-1:0] inPadded = {{(SIZE_POW2 - SIZE){1'b0}}, in};
 
 generate
 for (genvar i = 0; i <= NUM_STAGES; i=i+1) begin : tree
+    logic[(SIZE_POW2 >> i)*(i+1)-1:0] iSums;
+
+    if (i == 0)
+        assign iSums = inPadded;
+    else
+        for (genvar j = 0; j < (SIZE_POW2 >> i); j=j+1)
+                assign iSums[j*(i+1)+:(i+1)] = tree[i-1].iSums[2*j*i+:i] + tree[i-1].iSums[(2*j+1)*i+:i];
+
+end
+endgenerate
+
+// Nicer code (Yosys can't handle)
+/*
+generate
+for (genvar i = 0; i <= NUM_STAGES; i=i+1) begin : tree
     logic[(SIZE_POW2 >> i)-1:0][i:0] iSums;
 
     if (i == 0)
@@ -25,6 +40,7 @@ for (genvar i = 0; i <= NUM_STAGES; i=i+1) begin : tree
         end
 end
 endgenerate
+*/
 
 assign res = tree[NUM_STAGES].iSums;
 
