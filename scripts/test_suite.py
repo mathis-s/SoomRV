2b1b9e1069ddc33ddd1d8888c130c3155845be9a
Mathis Salmen <mathis.salmen@matsal.de>
2024-10-25 22:42:02 +0200

    test

--- scripts/test_suite.py
+++ scripts/test_suite.py
@@ -11,6 +11,8 @@ if len(sys.argv) > 1: test_dir = sys.argv[1]
 
 arr = os.popen(f"find {test_dir} -type f ! -size 0 -exec grep -IL . \"{{}}\" \\;").read().split('\n')
 
+print(arr)
+
 # we're running without a runtime, so no misaligned accesses.
 arr.remove(f"{test_dir}/rv32ui-p-ma_data")
 arr.remove(f"{test_dir}/rv32ui-v-ma_data")
