f8559d5f810cde3ba374bab877cabda143168003
Mathis Salmen <mathis.salmen@matsal.de>
2025-02-16 22:38:30 +0100

    fix

--- src/IssueQueue.sv
+++ src/IssueQueue.sv
@@ -241,7 +241,7 @@ always_ff@(posedge clk /*or posedge rst*/) begin
         newInsertIndex = 0;
         // Set insert index to first invalid entry
         for (integer i = 0; i < SIZE; i=i+1) begin
-            if (i < insertIndex && $signed(queue[i].sqN - IN_branch.sqN) <= 0) begin
+            if (i < insertIndex && $signed(queue[i].sqN - IN_branch.sqN) < 0) begin
                 newInsertIndex = i[$clog2(SIZE):0] + 1;
             end
         end
